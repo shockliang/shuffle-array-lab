b3454ba3d76102deb3a3840a75852f2bdc80cbb9
Shock Liang <shockwater@gmail.com>
2017-10-12 17:59:31 +0800

    Run many times to testing distribute region

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ShuffleArrayLab
 {
     class Program
     {
+        private static Dictionary<string, int> outcomeRecord = new Dictionary<string, int>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] ary = new int[] { 0, 1, 2 };
+
+            for (int i = 0; i < 6000000; i++)
+            {
+                ary.Shuffle();
+                WhichOutcome(ary);
+            }
+
+            PrintOutcomes();
+        }
+
+        private static void PrintOutcomes()
+        {
+            foreach (var item in outcomeRecord)
+            {
+                System.Console.WriteLine($"[{item.Key}]:{item.Value}");
+            }
+        }
+
+        private static void WhichOutcome(int[] ary)
+        {
+            string key = String.Join("-", ary);
+
+            if (outcomeRecord.ContainsKey(key))
+                outcomeRecord[key]++;
+            else
+                outcomeRecord.Add(key, 1);
         }
     }
 }
